9586642e74f0bb77bb5dff6c9e0a9e351abe3fa1
Nao1013 <121780890+Nao1013@users.noreply.github.com>
2023-02-13 02:21:34 +0000

    ログインできない。プロフィール画面実装中

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
  
-  resources :post_images, only:[:new, :create, :index, :show, :destroy]
+ resources :users, only: [:edit, :show, :update]
+  resources :post_images, only: [:new, :create, :index, :show, :destroy]
   devise_for :users
   get 'homes/about' => 'homes#about', as: 'about'
   root to: "homes#top"
